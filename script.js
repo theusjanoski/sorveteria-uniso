25c9b5022c00260d815216014934d83083726a68
Matheus Janoski <matheusjanoski012@gmail.com>
2024-03-27 22:51:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function SalvarDados(){
+    alert("Cadastro feito com sucesso.");
+    //INSERT INTO clientes (nome, cpf, email, endereco, telefone) VALUES
+}
+
+function LimparDados(){
+    document.getElementById('cadastroForm').reset();
+}
+
+function CancelarDados(){
+    alert("Cancelamento feito com sucesso.")
+}
